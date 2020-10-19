be5edc7218418c74e09146b1188bcc7bd5c4491e
student1043 <1043@holbertonschool.com>
2020-10-19 15:15:19 +0100

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Sum of List Items """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Sum of List Items """
+    return (sum(input_list))
