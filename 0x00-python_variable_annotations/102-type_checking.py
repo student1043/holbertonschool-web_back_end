474814e66c6db6a74b6b5c605cb0c0d20bb96e97
student1043 <1043@holbertonschool.com>
2020-10-19 16:05:45 +0100

    Fixed Tuples

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -12,8 +12,8 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     return zoomed_in
 
 
-array: Tuple = (12, 72, 91)
+array: = [12, 72, 91]
 
-zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(tuple(array))
 
-zoom_3x = zoom_array(array, int(3.0))
+zoom_3x = zoom_array(tuple(array), int(3.0))
