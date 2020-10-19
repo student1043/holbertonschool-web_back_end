822e1ecb075d4c43a07d75e7a889db9718aff39e
student1043 <1043@holbertonschool.com>
2020-10-19 15:19:12 +0100

    Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Sum of List Items """
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Sum of List Items """
+    return (sum(mxd_lst))
