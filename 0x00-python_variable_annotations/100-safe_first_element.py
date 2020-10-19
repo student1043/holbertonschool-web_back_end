23079d70798731b9991f6c70cd43efbe11d873b7
student1043 <1043@holbertonschool.com>
2020-10-19 15:49:40 +0100

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ from typing import Sequence, Union, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Element Length """
     if lst:
         return lst[0]
     else:
