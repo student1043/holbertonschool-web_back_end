6d690f08e3c86327aca745204f8a2ac500d68912
student1043 <1043@holbertonschool.com>
2020-10-20 19:35:18 +0100

    Fixing Float

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,12 +3,11 @@
 Test file for printing the correct output of the wait_n coroutine
 '''
 import asyncio
-from typing import List
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> List[float]:
+def measure_time(n: int, max_delay: int) -> float:
     '''
     Test file for printing the correct output of the wait_n coroutine
     '''
