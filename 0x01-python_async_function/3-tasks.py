58da8c963091fa3b982b9117f14bae124829afdb
student1043 <1043@holbertonschool.com>
2020-10-20 19:53:12 +0100

    Update 3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,11 +3,10 @@
 Test file for printing the correct output of the wait_n coroutine
 '''
 import asyncio
-import time
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     '''
     Test file for printing the correct output of the wait_n coroutine
     '''
