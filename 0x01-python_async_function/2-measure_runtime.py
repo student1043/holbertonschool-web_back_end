cb6cb7bd0573ced8ac48adb2b4516ce4836efa3b
student1043 <1043@holbertonschool.com>
2020-10-20 19:34:03 +0100

    Task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Test file for printing the correct output of the wait_n coroutine
+'''
+import asyncio
+from typing import List
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> List[float]:
+    '''
+    Test file for printing the correct output of the wait_n coroutine
+    '''
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time / n
