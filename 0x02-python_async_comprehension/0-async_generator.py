6f0087a9560b6c7fd2dfe9df29cc415c37e114f3
student1043 <1043@holbertonschool.com>
2020-10-21 14:34:22 +0100

    Fixed the output to float instead of Integer

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 """ Async Generator """
 import asyncio
 from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Async Generator """
     for i in range(10):
         yield uniform(0, 10)
