1e0345c4fb4a722b83f2fa5ec646e8b0fae97031
student1043 <1043@holbertonschool.com>
2020-10-21 14:57:19 +0100

    Task 3

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Measure Runtime """
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ Measure Runtime """
+    start = time.time()
+    await asyncio.gather(*[async_comprehension(), async_comprehension(),
+                          async_comprehension(), async_comprehension()])
+    total_runtime = time.time() - start
+    return total_runtime
