033ebd19fc1fbff614a8649aadf261cc11a2a22d
student1043 <1043@holbertonschool.com>
2020-10-21 14:42:52 +0100

    Return is a List not Generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Async Comprehension """
 import asyncio
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """ Async Comprehension """
     result = [i async for i in async_generator()]
     return result
