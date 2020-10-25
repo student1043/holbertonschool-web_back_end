400e2a211b9508a42ad4bcd03fd2706fb887b4f2
student1043 <1043@holbertonschool.com>
2020-10-25 19:16:19 +0100

    Fix 1

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -7,16 +7,14 @@ class BasicCache(BaseCaching):
     """ 0-basic_cache """
     def put(self, key, item):
         """ 0-basic_cache """
-        super(BasicCache, self).put(key, item)
-        if key or item is None:
+        if not key or not item:
             pass
         else:
-            self.cache_data['key'] = item
+            self.cache_data[key] = item
 
     def get(self, key):
         """ 0-basic_cache """
-        super(BasicCache, self).get(key)
-        if key or self.cache_data[key] is None:
+        if not key or not self.cache_data:
             return None
         else:
             return self.cache_data[key]
