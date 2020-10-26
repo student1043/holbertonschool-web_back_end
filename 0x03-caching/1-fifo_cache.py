a3e11c1801cad9515735d5de5b64c76baffc04a6
student1043 <1043@holbertonschool.com>
2020-10-26 20:24:33 +0100

    Update 1-fifo_cache.py

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-""" FIFOCache """
+""" FIFO """
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFOCache Class """
+    """ FIFO """
 
     def __init__(self):
-        """ constructor """
+        """ FIFO """
         super().__init__()
-        self.order = []
+        self.mylisting = []
 
     def put(self, key, item):
-        """
-        assign to the dictionary self.cache_data
-        the item value for the key key
-        """
+        """ FIFO """
         if key is not None and item is not None:
-            self.order.append(key)
+            self.mylisting.append(key)
             if key in self.cache_data:
                 self.cache_data[key] = item
-                self.order.remove(key)
+                self.mylisting.remove(key)
             else:
                 if len(self.cache_data) >= self.MAX_ITEMS:
-                    del self.cache_data[self.order[0]]
-                    print("DISCARD:", self.order[0])
-                    self.order.pop(0)
+                    del self.cache_data[self.mylisting[0]]
+                    print("DISCARD:", self.mylisting[0])
+                    self.mylisting.pop(0)
                 self.cache_data[key] = item
 
     def get(self, key):
-        """
-        return the value of key in self.cache_data
-        """
+        """ FIFO """
         if key in self.cache_data:
             return self.cache_data[key]
         return None
