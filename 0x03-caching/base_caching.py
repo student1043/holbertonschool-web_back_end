66fda98345407698198dd949cc1d4214af6af004
student1043 <1043@holbertonschool.com>
2020-10-26 20:16:00 +0100

    Update base_caching.py

--- 0x03-caching/base_caching.py
+++ 0x03-caching/base_caching.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" BaseCaching module
 """
+BaseCaching module
+"""
+
 
 class BaseCaching():
     """ BaseCaching defines:
@@ -22,11 +24,13 @@ class BaseCaching():
             print("{}: {}".format(key, self.cache_data.get(key)))
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """
+        Add an item in the cache
         """
         raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
-        """ Get an item by key
+        """
+        Get an item by key
         """
         raise NotImplementedError("get must be implemented in your cache class")
