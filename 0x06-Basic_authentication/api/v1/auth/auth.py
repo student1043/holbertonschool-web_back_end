7ff17c1ef5d64657e5ff2f591ff6f2282edfc52f
student1043 <1043@holbertonschool.com>
2020-11-06 21:55:50 +0100

    Task 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,16 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Requirement for Authentication
         """
-        return False
+        if excluded_paths is not None and len(excluded_paths) > 0:
+            for i in excluded_paths:
+                if i.__contains__(path):
+                    return False
+        if path is None or path not in excluded_paths:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """ Authorization Header
