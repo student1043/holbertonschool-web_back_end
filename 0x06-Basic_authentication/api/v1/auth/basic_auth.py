2c6be9ff31d87759c428f1499b2d79189d42c103
student1043 <1043@holbertonschool.com>
2020-11-06 23:27:32 +0100

    Task 7

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,8 +4,20 @@
 from api.v1.auth.auth import Auth
 from flask import request
 from typing import List, TypeVar
+import base64
 
 
 class BasicAuth(Auth):
     """ Basic Authentication
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extracting BASE64
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return(authorization_header[6:])
