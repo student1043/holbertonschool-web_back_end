31b5b6b0303315a996346632df6acf8b6e65bc0f
student1043 <1043@holbertonschool.com>
2021-01-11 22:49:18 +0100

    Update 1-block-scoped.js

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -5,6 +5,9 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     task = true;
     task2 = false;
+  } else {
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
