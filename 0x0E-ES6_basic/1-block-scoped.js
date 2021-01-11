effacd95f19332ad64d6f0dfc15d933ffcd3200c
student1043 <1043@holbertonschool.com>
2021-01-11 22:54:46 +0100

    Update 1-block-scoped.js

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,13 +1,14 @@
+/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-  let task;
-  let task2;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = false;
+    const task2 = true;
   } else {
-    task = true;
-    task2 = false;
+    const task = false;
+    const task2 = true;
   }
 
   return [task, task2];
