79384d61d835e891289363d083cbe2fbbd230efd
student1043 <1043@holbertonschool.com>
2021-01-12 22:15:17 +0100

    Update 9-getFullBudget.js

--- 0x0E-ES6_basic/9-getFullBudget.js
+++ 0x0E-ES6_basic/9-getFullBudget.js
@@ -5,8 +5,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: () => `$${income}`,
-    getIncomeInEuros: () => `${income} euros`,
+    getIncomeInDollars: () => `$` + income,
+    getIncomeInEuros: () => income + ` euros`,
   };
 
   return fullBudget;
