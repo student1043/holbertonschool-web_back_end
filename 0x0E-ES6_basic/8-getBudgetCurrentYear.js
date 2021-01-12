e89f095effff011cacb060e104222e06b29d15b7
student1043 <1043@holbertonschool.com>
2021-01-12 20:33:35 +0100

    Update 8-getBudgetCurrentYear.js

--- 0x0E-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x0E-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,15 +1,13 @@
-/* eslint-disable */
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear() - 1;
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  budget[`income-` + getCurrentYear()] = income;
-  budget[`gdp-` + getCurrentYear()] = gdp;
-  budget[`capita-` + getCurrentYear()] = capita;
-
+  const budget = {
+    [`income-` + getCurrentYear()]: income,
+    [`gdp-` + getCurrentYear()]: gdp,
+    [`capita-` + getCurrentYear()]: capita
+  }
   return budget;
 }
