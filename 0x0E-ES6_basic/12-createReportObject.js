dcad195cd398dae56d87b84b37fff9345735c0ae
student1043 <1043@holbertonschool.com>
2021-01-16 15:28:19 +0100

    Update 12-createReportObject.js

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   const employees = {
-    AllEmployees: employeesList,
-    getNumberOfDepartments: (employeesList) => `${employeesList}`
+    AllEmployees: ...employeesList,
+    getNumberOfDepartments: Object.keys(employees).length
   };
 
   return employees;
