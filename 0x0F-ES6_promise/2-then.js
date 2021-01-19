1c294200053601b1f94638828cecc9fbd648794b
student1043 <1043@holbertonschool.com>
2021-01-19 15:40:56 +0100

    Update 2-then.js

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,9 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  Promise.all(console.log('Got a response from the API'));
   return promise.then(() => ({
     status: 200,
     body: 'status',
-
-  }
-  )).catch(new Error(''));
+  })).catch(new Error(''))
+    .finally('Got a response from the API');
 }
