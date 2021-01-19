2e259f7d4306c70d4e8c258f6d931ca5a12d14b2
student1043 <1043@holbertonschool.com>
2021-01-19 13:42:02 +0100

    Task 2

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({
+        status: 200,
+        body: 'success',
+      });
+      console.log('Got a response from the API');
+    } else {
+      reject(new Error());
+      console.log('Got a response from the API');
+    }
+  });
+}
