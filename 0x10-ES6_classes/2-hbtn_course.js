3a6470333ef756aa6fa04d20ce75cf67a1d89d48
student1043 <1043@holbertonschool.com>
2021-01-20 21:52:25 +0100

    Update 2-hbtn_course.js

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,23 +1,31 @@
 export default class HolbertonCourse {
-    constructor(name = String, length = Number, students = Array) {
-        this._name = name;
-        this._length = length;
-        this._students = students;
-    }
-    get name() {
-    }
-    set name(name) {
-        this._name = name;
-    }
-    get length() {
-    }
-    set length(length) {
-        this._length = length;
-    }
-    get students() {
+  constructor(name = String, length = Number, students = Array) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    }
-    set students(students) {
-        this._students = students;
-    }
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
 }
