994474d56a5db498dd87a5233fc58744477881ea
student1043 <1043@holbertonschool.com>
2021-01-20 19:05:26 +0100

    Task 0

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,6 +3,6 @@ import createUser from "./utils";
 
 export default function handleProfileSignup() {
     Promise.all([uploadPhoto, createUser]).then((elements) => {
-        console.log(elements);
+        console.log(elements[0]);
     })
 }
