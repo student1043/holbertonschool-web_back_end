6fab42c78c0a6b11bec25312441400694605a232
student1043 <1043@holbertonschool.com>
2021-01-22 20:40:43 +0100

    Update 4-update_grade_by_city.js

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,18 @@
+/* eslint-disable */
 export default function updateStudentGradeByCity(input, city, newGrades) {
-    return input.filter((localization) => localization.location === city).map(
-        newGrades.map(((person) => (person.studentId)))
-    )
+  return input.filter((localization) => localization.location === city).map(
+    (student) => {
+      let found = false;
+      for (const mystudents of newGrades) {
+        if (student.id === mystudents.studentId) {
+          student.grade = mystudents.grade;
+          found = true;
+        }
+      }
+      if (found === false) {
+        student.grade = 'N/A';
+      }
+      return student;
+    },
+  );
 }
