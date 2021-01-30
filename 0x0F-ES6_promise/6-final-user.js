85d8753ba99d7e569fbc2e708ca1ff2be695f5a9
student1043 <1043@holbertonschool.com>
2021-01-30 23:03:17 +0100

    Update 6-final-user.js

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,11 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return new Promise((resolve) => {
-    resolve(signUpUser(firstName, lastName));
-    resolve(uploadPhoto(fileName));
-    return [
-
-    ]
-  }).catch((error) => (error));
+  return new Promise((resolve) => [resolve(signUpUser(firstName, lastName)),
+    resolve(uploadPhoto(fileName))]).catch((error) => (error));
 }
