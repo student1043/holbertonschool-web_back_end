c4f28b89edbf760708af91e3193a83a5a5393336
student1043 <1043@holbertonschool.com>
2021-02-04 00:45:57 +0100

    Update 8-all.py

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ 8-all """
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-    return(list(mongo_collection.find({})))
+    """ listing all collection items """
+    cursor = (list(mongo_collection.find({})))
+    for document in cursor.find():
+        return document
