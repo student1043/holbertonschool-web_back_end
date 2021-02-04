8cea286338b2ac3ea22b3e7369299ee93ca351f5
student1043 <1043@holbertonschool.com>
2021-02-04 18:53:22 +0100

    Update 0-app.py

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+""" i18n project """
 from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def home():
+    """ i18n project """
     return render_template('0-index.html')
