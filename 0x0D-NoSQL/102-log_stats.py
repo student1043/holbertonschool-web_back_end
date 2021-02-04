e65f4a798e898281940a62ae0a876718db422593
student1043 <1043@holbertonschool.com>
2021-02-04 17:57:17 +0100

    Create 102-log_stats.py

---
+++ 0x0D-NoSQL/102-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" 12-log_stats """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    connection = client.logs.nginx
+    numofcollections = len(list(connection.find()))
+    print(numofcollections, "logs")
+    print("Methods:")
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for i in method:
+        print("\tmethod", i + ":", len(list(connection.find({"method": i}))))
+
+    print(len(list(connection.find({"method": "GET", "path": "/status"}))),
+          "status check")
+
+    print("IPs:")
+    for ips in connection:
+        print(ips)
