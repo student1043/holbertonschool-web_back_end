90b86436a8fe8f31c304ae99b50f419d7e68b62a
student1043 <1043@holbertonschool.com>
2021-02-05 15:28:08 +0100

    Task 2

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" i18n project """
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='templates')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['fr', 'en'])
+
+
+@app.route('/')
+def home():
+    """ i18n project """
+    return render_template('2-index.html')
