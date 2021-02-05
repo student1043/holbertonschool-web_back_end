6931325edbed8ccea5943a34cb38077477d43116
student1043 <1043@holbertonschool.com>
2021-02-05 15:38:06 +0100

    Update 2-app.py

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -6,13 +6,21 @@ from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='templates')
 
 
-@babel.localeselector
-def get_locale():
-    """ get locale language"""
-    return request.accept_languages.best_match(['fr', 'en'])
+class Config:
+    """ Config Class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 babel = Babel(app)
+app.config.from_object(Config())
+
+
+@babel.localeselector
+def get_locale():
+    """ get locale language"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
