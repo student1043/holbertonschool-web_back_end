de9f3ded2bb55d1e12552b3959e3c8b2462193ba
student1043 <1043@holbertonschool.com>
2021-02-05 15:51:35 +0100

    Task 3

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" i18n project """
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='templates')
+
+
+class Config:
+    """ Config Class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+babel = Babel(app)
+app.config.from_object(Config())
+
+
+@babel.localeselector
+def get_locale():
+    """ get locale language"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def home():
+    """ i18n project """
+    return render_template('2-index.html')
