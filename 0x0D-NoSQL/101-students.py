39ed2a179abed207ddf127983fd42797d108f4fa
student1043 <1043@holbertonschool.com>
2021-02-06 21:32:33 +0100

    Update 101-students.py

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -8,11 +8,11 @@ def top_students(mongo_collection):
     studentlist = list(mongo_collection.find())
     students = []
     for student in studentlist:
-        mystudent = {"name": student.get("name"), "_id": student.get("_id"), "averageScore": 0}
+        mystudent = {"name": student.get("name"), "_id": student.get("_id")}
         total = 0
         for myscore in student.get("topics"):
             total += myscore.get("score")
         mystudent["averageScore"] = total / len(student.get("topics"))
         students.append(mystudent)
-    sorted(students, key = lambda i: i['averageScore'], reverse = True)
+    sorted(students, key = lambda i: i["averageScore"], reverse = True)
     return students
