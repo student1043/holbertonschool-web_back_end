9c7688e920fcb0870661445f39c243a07a306805
student1043 <1043@holbertonschool.com>
2021-02-06 01:53:13 +0100

    Task 5

---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+""" i18n project """
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """
+    Config Class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config())
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get locale language
+    """
+    if request.url[-2:] in app.config["LANGUAGES"]:
+        return request.url[-2:]
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+def get_user():
+    """
+    getting user ID
+    """
+    if request.url[-10:-2] == "login_as" or (request.url[-1:]) in users:
+        return users[int(request.url[-1:])]
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    before request
+    """
+    g.user = get_user()
+
+
+@app.route('/')
+def home():
+    """
+    i18n project
+    """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
