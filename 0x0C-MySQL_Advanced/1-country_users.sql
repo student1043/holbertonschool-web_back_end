ac3b5acb7de92fa82198da636ce21080584a9948
student1043 <1043@holbertonschool.com>
2021-02-06 19:05:12 +0100

    Create 1-country_users.sql

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,5 @@
+-- Second Task
+-- MySQL Advanced
+CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255),
+                    country ENUM('US', 'CO', 'TN') NOT NULL);
