d55296ae8de564bfc6927f1aa58449632dac9e56
student1043 <1043@holbertonschool.com>
2021-02-06 23:08:03 +0100

    Update 100-weak.js

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,11 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const number = weakMap.get(endpoint);
-  if (number === 0) {
+  if (number) {
     weakMap.set(endpoint, 1);
   } else {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   }
-
   if (number >= 5) {
     throw Error('Endpoint load is high');
   }
