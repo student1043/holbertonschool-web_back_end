079708e055deb1ddf299e2179b0483d48db2b830
student1043 <1043@holbertonschool.com>
2021-02-06 22:24:45 +0100

    Update 100-await.js

--- 0x0F-ES6_promise/100-await.js
+++ 0x0F-ES6_promise/100-await.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default async function asyncUploadUser() {
     let myobject
     try{(myobject = { photo: await uploadPhoto(),
-        user: await signUpUser()}).catch(myobject = { photo: null,
-        user: null})
-        return myobject
-    }
+        user: await signUpUser()})}
+    catch{(myobject = { photo: null,
+        user: null})}
+    return myobject
 }
