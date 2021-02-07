ec91f86185d8d1c34ab963f0a1f9281b6cf3230c
student1043 <1043@holbertonschool.com>
2021-02-07 13:25:37 +0100

    Fixing

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -3,10 +3,9 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    console.log(`Your name is: ${chunk}`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
-
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
